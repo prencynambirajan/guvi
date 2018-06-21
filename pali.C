a982fe979bd33034c342a2f5f56d83c0cf7bd5af
prencynambirajan <35619000+prencynambirajan@users.noreply.github.com>
2018-06-21 14:59:16 +0530

    Create pali.C

---
+++ pali.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+int n, rev = 0, or;
+scanf("%d", &n);
+or = n;
+while( n!=0 )
+    {        
+        rev = rev*10 + n%10;;
+        n /= 10;
+    }
+    if (or == rev)
+        printf("yes");
+    else
+        printf("no"); 
+    return 0;
+}
